3f812ed9139d2522e46274053e1f857f0f4e4ed7
Loïc Dumay <dumay.famille@gmail.com>
2022-01-18 14:24:13 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'esiee_2022_angular_learn';
+}
